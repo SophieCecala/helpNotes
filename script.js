079077227c7b09ca0450747d293e92f5dfa8feea
Natalya Myunster <natalyamyunster@yahoo.com>
2021-03-31 22:10:35 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,47 @@
+
+//task 1
+// const discount = 28.99;
+// const price = 100;
+// let total = price - discount;
+//  if(total < 25){
+//      alert("positive credit card balance");
+//  }
+
+ //task 2
+ // let question = 10;
+ // while( question > 0){
+ //     question--;
+ //     prompt(`i have already told you ${question} times`);
+ // }
+
+ //task 3
+ // let dog = { name:'Dexter', weight: 40};
+ // let dogWeight = dog.weight;
+ // let dogName = dog.name;
+ //
+ // if(dogWeight > 30){
+ //     alert('WOF WOF');
+ // } else {
+ //     alert('wof wof');
+ // }
+
+ //task 5
+//1.
+const temp = 10;
+const answer = (9 / 5) * temp  + 32;
+console.log(answer);
+
+//2.
+const color = 'orange';
+console.log(color === 'orange');
+
+//task 6
+
+var name = "Jhon";
+var i = 0;
+
+
+while (i < 2){
+    i++;
+    document.write(`Happy Birthday ${name}`);
+}
